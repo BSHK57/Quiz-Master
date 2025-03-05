7e71df3e6bc4f63d435d9fb9a1a56dde2189e714
B SAI HARI KRISHNA <166925264+BSHK57@users.noreply.github.com>
2025-03-05 10:27:39 +0530

    Delete Students directory

--- Students/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Student,Subject
-# Register your models here.
-admin.site.register(Student)
-admin.site.register(Subject)
