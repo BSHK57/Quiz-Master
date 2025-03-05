917e3686dedbef7c7bc969877a0583288268ada5
B SAI HARI KRISHNA <166925264+BSHK57@users.noreply.github.com>
2025-03-05 10:27:19 +0530

    Delete Quizzes directory

--- Quizzes/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('quiz_dashboard/', views.quiz_dashboard, name='quiz_dashboard'),
-    path('leaderboard/', views.leaderboard, name='leaderboard'),
-    path('download_report/', views.download_report, name='download_report'),
-]
